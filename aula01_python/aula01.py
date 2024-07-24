efec4800dbc4c91f301890384dedbbd9ffc05719
Hyran Teixeira <hyranteixeirastd@gmail.com>
2024-07-24 01:24:07 -0300

    Adicionar arquivo da aula

---
+++ aula01_python/aula01.py
@@ -0,0 +1,20 @@
+# Exercício 01
+'''
+Crie um programa que o usuário diga o seu nome e retorna o número de caracteres
+'''
+
+nome = input("Digite o seu nome: ")
+caracteres_nome = len(nome)
+
+print(f"O seu nome tem {caracteres_nome} caracteres")
+
+# Exercício 02
+'''
+Crie um programa onde o usuário digite dois valores e apareça a soma
+'''
+
+valor_01 = int(input("Digite o primeiro valor: "))
+valor_02 = int(input("Digite o segundo valor: "))
+soma_valores = valor_01 + valor_02
+
+print(f"A soma dos valores é {soma_valores}")
